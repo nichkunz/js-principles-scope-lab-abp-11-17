63e29b6272c7db753ecd5c9da7065bada250ed66
nicholas kunz <mrnicholaskunz@gmail.com>
2017-11-09 17:14:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your solution in this file!
 var customerName = 'bob';
 
-function upperCaseCustomerName(makingBig) {
+function upperCaseCustomerName() {
   let makingBig = customerName.toUpperCase();
 }
