815708120d6bcd4754ccabc406f6f7d3adf01540
nicholas kunz <mrnicholaskunz@gmail.com>
2017-11-09 17:10:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your solution in this file!
 var customerName = 'bob';
 
-function upperCaseCustomerName(customerName) {
-  customerName = customerName.upperCase.
+function upperCaseCustomerName(customerNames) {
+  customerNames = customerName.upperCase
 }
